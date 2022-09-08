ad32bfdb66b419275ef1036cae87e08579c3c8b3
Nadia Paz <nadina.abouhamdan@gmail.com>
2022-09-08 15:59:33 -0500

    added correct solutions

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -17,15 +17,31 @@ WHERE hire_date LIKE '199%'
 
 SELECT first_name, last_name
 FROM employees
-WHERE hire_date LIKE '199%'
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25'
 ORDER BY hire_date
 LIMIT 5;
 
--- 'Aiman', 'Hainaut'
--- 'Lillian', 'Stiles'
--- 'Teruyuki', 'Sridhar'
--- 'Sashi', 'Demeyer'
--- 'Tomofumi', 'Rattan'
+-- Alselm, Cappello
+-- Utz, Mandell
+-- Bouchung, Schreiter
+-- Baocai, Kushner
+-- Petter, Stroustrup
+
+-- Page #10
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25'
+ORDER BY hire_date
+LIMIT 5 OFFSET 45;
+
+-- 'Pranay', 'Narwekar'
+-- 'Marjo', 'Farrow'
+-- 'Ennio', 'Karcich'
+-- 'Dines', 'Lubachevsky'
+-- 'Ipke', 'Fontan'
+
 
 -- What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
 PAGE = OFFSET / LIMIT + 1
+OFFSET = (PAGE - 1) * LIMIT
