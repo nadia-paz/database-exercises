519d4f94554e3727f8e87621fc984fd2fc873396
Nadia Paz <nadina.abouhamdan@gmail.com>
2022-09-08 15:27:13 -0500

    limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,29 @@
+-- 1. List the first 10 distinct last name sorted in descending order.
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- Find all previous or current employees hired in the 90s and born on Christmas.
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+	AND birth_date LIKE '%12-25';
+
+ 
+-- Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. 
+-- Write a comment in your code that lists the five names of the employees returned.
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%'
+ORDER BY hire_date
+LIMIT 5;
+
+-- 'Aiman', 'Hainaut'
+-- 'Lillian', 'Stiles'
+-- 'Teruyuki', 'Sridhar'
+-- 'Sashi', 'Demeyer'
+-- 'Tomofumi', 'Rattan'
+
