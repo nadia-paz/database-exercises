6acde0b4c1a06bb15a046744a0fbada9c49072d4
Nadia Paz <nadina.abouhamdan@gmail.com>
2022-09-13 13:55:24 -0500

    add bonus exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -168,9 +168,39 @@ SELECT (
 -- BONUS
 #1
 -- Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+				SELECT dept_no
+				FROM dept_manager
+				WHERE to_date > CURDATE() AND emp_no IN (
+								SELECT emp_no
+								FROM employees 
+								WHERE gender = 'F'
+								)
+); -- 'Development','Finance', 'Human Resources''Research'
 
 #2
 -- Find the first and last name of the employee with the highest salary.
-
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (
+		SELECT emp_no
+		FROM salaries
+		WHERE salary = (SELECT MAX(salary) FROM salaries)
+);
+-- Tokuyasu Pesch
 #3
 --Find the department name that the employee with the highest salary works in.
+SELECT dept_name
+FROM departments
+WHERE dept_no = (
+SELECT dept_no
+FROM dept_emp
+WHERE emp_no = (
+				SELECT emp_no
+				FROM salaries
+				WHERE salary = (SELECT MAX(salary) FROM salaries)
+		)
+);
+-- Sales
