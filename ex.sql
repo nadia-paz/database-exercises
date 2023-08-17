98a563577ad8c582bf23abdce99aeb95f6520f84
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-08-17 13:53:19 -0500

    add 24

--- ex.sql
+++ ex.sql
@@ -212,5 +212,20 @@ FROM Product
 JOIN Laptop l USING(model)
 WHERE l.speed >= 750;
 
+/*
+24. Перечислите номера моделей любых типов, 
+имеющих самую высокую цену по всей имеющейся в базе данных продукции.
+*/
+WITH un_table AS
+(
+    SELECT model, price FROM PC WHERE price = (SELECT MAX(price) FROM PC)
+    UNION
+    SELECT model, price FROM Laptop WHERE price = (SELECT MAX(price) FROM Laptop)
+    UNION
+    SELECT model, price FROM Printer WHERE price = (SELECT MAX(price) FROM Printer)
+)
+SELECT model
+FROM un_table
+WHERE price = (SELECT MAX(price) FROM un_table);
 
 
