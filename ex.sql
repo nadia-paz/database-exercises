796c8f4d6f3666198d23311702958bd34d0ade2b
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-08-21 14:13:50 -0500

    add description

--- ex.sql
+++ ex.sql
@@ -43,6 +43,27 @@ Notes:
 3) For historical reasons, lead ships are referred to as head ships in many exercises.
 4) A ship found in the Outcomes table but not in the Ships table is still considered in the database. 
 This is true even if it is sunk.
+
+Recycling firm
+
+Income (code-int, point - tinyint, date - datetime, inc - smallmoney) PK = code
+Outcome (code-int, point - tinyint, date - datetime, out - smallmoney) PK = code
+Income_o (point, date, inc) PK = (point, date)
+Outcome_0 (point, date, out) PK = (point, date)
+
+The firm owns several buy-back centers for collection of recyclable materials. 
+Each of them receives funds to be paid to the recyclables suppliers. 
+Data on funds received is recorded in the table Income_o(point, date, inc)
+The primary key is (point, date), where point holds the identifier of the buy-back center, 
+and date corresponds to the calendar date the funds were received. 
+The date column doesn’t include the time part, thus, money (inc) arrives no more than once a day for each center. 
+Information on payments to the recyclables suppliers is held in the table Outcome_o(point, date, out)
+In this table, the primary key (point, date) ensures each buy-back center reports about payments (out) no more than once a day, too.
+For the case income and expenditure may occur more than once a day, 
+another database schema with tables having a primary key consisting of the single column code is used:
+Income(code, point, date, inc)
+Outcome(code, point, date, out)
+Here, the date column doesn’t include the time part, either.
 */
 
 /* 
