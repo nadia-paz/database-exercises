03364df300639f24e06d98061f4a9cc92b09d125
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-09-06 16:53:30 -0500

    add correct BST solution

--- hr_1.sql
+++ hr_1.sql
@@ -4,17 +4,11 @@ N - number
 P - parent
 print out a number and "Leaf/Inner/Root"
 */
-SELECT N, CASE
-WHEN P IS IN (SELECT DISTINCT P FROM BST WHERE P IS NULL) THEN "Root"
-WHEN N IS NOT IN (SELECT DISTINCT P FROM BST) THEN "Leaf"
-ELSE "Inner" END 
-FROM BST
-ORDER BY N;
-/*or*/
-SELECT N, CASE
-WHEN P IS NULL THEN "Root"
-WHEN N IS IN (SELECT DISTINCT P FROM BST) THEN "Inner"
-ELSE "Leaf" END 
+SELECT DISTINCT N, 
+    CASE
+        WHEN N IN (SELECT N FROM BST WHERE P IS NULL) THEN 'Root'
+        WHEN N IN (SELECT DISTINCT P FROM BST) THEN 'Inner'
+        ELSE 'Leaf' END node
 FROM BST
 ORDER BY N;
 
@@ -48,3 +42,54 @@ WHERE UPPER(CITY) LIKE 'A%' OR
     UPPER(CITY) LIKE 'I%' OR 
     UPPER(CITY) LIKE 'O%' OR 
     UPPER(CITY) LIKE 'U%';
+
+/* WOS 7
+Query the list of CITY names ending with vowels (a, e, i, o, u) 
+from STATION. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(LOWER(CITY), 1) IN (
+    'a', 'e', 'i', 'o', 'u'
+);
+
+/* WOS 8
+Last and first letters are vowels
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(LOWER(CITY), 1) IN (
+    'a', 'e', 'i', 'o', 'u'
+)
+AND LEFT(LOWER(CITY), 1) IN (
+    'a', 'e', 'i', 'o', 'u'
+);
+
+/* WOS 9 
+Doesn't start with vowel
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(LOWER(CITY), 1) NOT IN (
+    'a', 'e', 'i', 'o', 'u'
+);
+
+/* WOS 10 
+Doesn't end with vowel
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(LOWER(CITY), 1) NOT IN (
+    'a', 'e', 'i', 'o', 'u'
+);
+
+/*
+Query the Name of any student in STUDENTS who scored higher than  Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters 
+(i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*/
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(NAME, 3), ID;
